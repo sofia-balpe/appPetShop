a4377f65489317a40e4609242c6c9394e626477d
Sofia Baldissera Pereira <sofia.bp0511@gmail.com>
2025-10-09 21:45:09 -0400

    atualizações

--- appPetShop/obj/Debug/net8.0/appPetShop.AssemblyInfo.cs
+++ appPetShop/obj/Debug/net8.0/appPetShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("appPetShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e28b8c3cf70d6d0731ae72b645861d4fcf21369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7d5a477e2ecba1b54b137c4dd71e6e6d4c7037")]
 [assembly: System.Reflection.AssemblyProductAttribute("appPetShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("appPetShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
